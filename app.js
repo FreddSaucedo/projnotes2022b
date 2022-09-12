03b1dc3249db964b0f18489c69ba0264e758602d
FreddSaucedo <alfredosaucedorangel@gmail.com>
2022-09-12 08:50:16 -0500

    Comentarios Se agregaron comentarios en app.js

--- app.js
+++ app.js
@@ -1,25 +1,51 @@
+//Biblioteca 
 var createError = require('http-errors');
+//El framework express
 var express = require('express');
+//Bibliooteca del nucleo de node que sirve para
+//administrar rutas
 var path = require('path');
+//Biblioteca externa que sirve para administrar cookies
 var cookieParser = require('cookie-parser');
+//Biblioteca que registra en consola
+//solicitudes del cliente
 var logger = require('morgan');
 
+//Definición de rutas
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
+//view engine setup
+//Configura el motor de plantillas
+//1. Establecer donde estarán las plantillas
+///vistas -> Views
+//app.set("<nombre de la var>", <valor>)
 app.set('views', path.join(__dirname, 'views'));
+console.log(`☢️ El valor de __dinrma es: ${__dirname}`)
+console.log(`⚠️ El valor del join es: ${path.join(__dirname, 'views')}}`)
+//JOIN TIENE LA INTELIGENCIA PARA SABER EN QUE ENTORNO ESTA Y SELECCIONA EL CARACTER CON EL QUE SELECCIONA LA RUTA ("/ O backslash")
+//Establezco que motor precargado usare
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//[Cliente] => {get+url+cliente+...} REQUEST
+//[Cliente] => req => 💻 => req (req,res)=>{} [ middleware 1 ] => req => [ middleware 2 ] => res
+//re == {html + dns cliente}
+//[Cliente] <== <== <== <== <== <== <== 
+//Middleware son objetos que reciben req y res next invoca al siguiente Middleware
 
-app.use('/', indexRouter);
+//Establezo Middelware
+app.use(logger('dev'));//logear
+app.use(express.json());//Middleware para parsear a json
+app.use(express.urlencoded({ extended: false }));// decode la URL
+app.use(cookieParser());//trabajar con cookies - parsear cookies, se guardan en el navegador
+app.use(express.static(path.join(__dirname, 'public')));//Servidor de archivos estaticos EN CARPETA public se puede servir todo de manera estatica
+
+
+//Registro Rutas
+app.use('/', indexRouter); 
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
