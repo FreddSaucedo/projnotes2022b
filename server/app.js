d7297ad72d256af5b29406612638fd444c41a5d2
FreddSaucedo <alfredosaucedorangel@gmail.com>
2022-10-31 07:33:35 -0600

    Rectificando código

--- server/app.js
+++ server/app.js
@@ -13,50 +13,52 @@ import cookieParser from 'cookie-parser';
 // Biblioteca que registra en consola
 // solicitudes del cliente
 import logger from 'morgan';
-import debug from './services/debugLogger'
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import WebpackHotMiddleware from 'webpack-hot-middleware';
+import debug from './services/debugLogger';
 
 // Importando Webbpack middleware
-import webpack from 'webpack'
-import webpackDevMiddleware from 'webpack-dev-middleware'
-import webpackHotMiddleware from 'webpack-hot-middleware'
-import webpackConfig from '../webpack.dev.config'
-
-// Recuperar el modo de ejecución de la app
-const nodeEnv = process.env.NODE_ENV || 'development'
+import webpackConfig from '../webpack.dev.config';
 
 // Definición de rutas
-import indexRouter from "./routes/index";
-import usersRouter from "./routes/users";
-import WebpackHotMiddleware from 'webpack-hot-middleware';
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+
+// Recuperar el modo de ejecución de la app
+const nodeEnv = process.env.NODE_ENV || 'development';
 
 // Creando una instancia de express
 const app = express();
 
 // Inclusion del webpack middleware
 if (nodeEnv === 'development') {
-  debug('✒ Ejecutando en modo de desarrollo 👨‍💻')
+  debug('✒ Ejecutando en modo de desarrollo 👨‍💻');
   // Configurando webpack en modo de desarrollo
-  webpackConfig.mode = 'development'
+  webpackConfig.mode = 'development';
   // Configurar la ruta del HMR (Hot Module Replacement)
   // 👉 "reload=true" -> Habilita la recarga automatica cuando un archivo
   // js cambia
   // 👉 "timeout=1000" -> Establece el timpo de refresco de la pagina
   webpackConfig.entry = [
-    "webpack-hot-middleware/client?reload=true&timeout=1000",
-    webpackConfig.entry
-  ]
+    'webpack-hot-middleware/client?reload=true&timeout=1000',
+    webpackConfig.entry,
+  ];
   // Agregando el plugin a la configuracion
-  webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin())
+  webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
   // Crear el empaquetado con webpack
   const bundler = webpack(webpackConfig);
   // Registro el middleware en express
-  app.use(webpackDevMiddleware(bundler, {
-    publicPath: webpackConfig.output.publicPath
-  }))
+  app.use(
+    webpackDevMiddleware(bundler, {
+      publicPath: webpackConfig.output.publicPath,
+    })
+  );
   // Registrando el HMR Middleware
-  app.use(WebpackHotMiddleware(bundler))
+  app.use(WebpackHotMiddleware(bundler));
 } else {
-  debug('✒ Ejecutando en modo de producción 🏭')
+  debug('✒ Ejecutando en modo de producción 🏭');
 }
 
 // view engine setup
@@ -69,7 +71,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 // Establezco Middelware
-app.use((logger('dev')));
+app.use(logger('dev'));
 // Middleware para parsear a json la peticion
 app.use(express.json());
 // Decodificar la url
@@ -77,7 +79,7 @@ app.use(express.urlencoded({ extended: false }));
 // Parsear cookies
 app.use(cookieParser());
 // Servidor de archivos estáticos
-app.use(express.static(path.join(__dirname,'..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Registro Rutas
 app.use('/', indexRouter);
@@ -85,12 +87,12 @@ app.use('/index', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use((req, res, next)=> {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use((err, req, res, next)=> {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
