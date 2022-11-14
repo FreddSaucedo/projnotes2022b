aaebd652c92e1b8bfa1c543fc1643c2c7c215b4a
FreddSaucedo <alfredosaucedorangel@gmail.com>
2022-11-13 19:40:14 -0600

    Agregando about 😀

--- server/home/homeController.js
+++ server/home/homeController.js
@@ -5,12 +5,20 @@ const home = (req, res) => {
   // 1. Generando el view-model
   const viewModel = {
     title: 'Express',
-    author: 'Ivan Rivalcoba',
+    author: 'Alfredo Saucedo',
   };
 
   // 2. Madamos a generar la vista con el Template Engine
   res.render('home', viewModel);
 };
 
+const about = (req, res) => {
+  // 1. Generando el view-model
+  const viewModel = {};
+
+  // 2. Madamos a generar la vista con el Template Engine
+  res.render('about', viewModel);
+};
+
 // Exportando el Controlador
-export default { home };
+export default { home, about };
